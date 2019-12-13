c3cc0e2cd34c959a0a27137bcc8d800fa970a11e
ShirokoMax <56560768+ShirokoMax@users.noreply.github.com>
2019-12-13 20:45:32 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-coefficient.oninput = calculate;
-force.oninput = calculate;
-diameter.oninput = calculate;
+let inputs = document.querySelectorAll('input'); // массив всех инпутов
+inputs.forEach( function(input) {
+  input.addEventListener("input", calculate);
+} )
 
 function calculate () {
     let k = document.getElementById('coefficient').value;
     let p = document.getElementById('force').value;
     let d = document.getElementById('diameter').value;
-    let result = k*p*1000*d/1000;
+    let result = Math.ceil( k*p*1000*d/1000 );
     document.getElementById('result').value = result;
  }
